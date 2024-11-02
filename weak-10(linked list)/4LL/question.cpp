a4dfdf8289117c351a9c740cf78308d9af3cb8ab
deepak-pandey1 <deepakpandeyofficially@gmail.com>
2024-11-02 20:10:24 +0530

    final ll

--- weak-10(linked list)/4LL/question.cpp
+++ weak-10(linked list)/4LL/question.cpp
@@ -17,12 +17,15 @@
 //     return false;
 // }
 
-//------------------------------------------------------->LL in loop where start
+
+
+//------------------------------------------------------->starting point of loop(leetcode 142)
 // ListNode *detectCycle(ListNode *head) {
+//     //step1:check for loop
 //     ListNode* slow = head;
 //     ListNode* fast = head;
 
-//     while(fast != NULL){             //---->1step to check cyle present or not
+//     while(fast != NULL){
 //         fast = fast->next;
 //         if(fast != NULL){
 //             fast = fast->next;
@@ -31,25 +34,91 @@
 //         if(fast == slow) break;
 //     }
 
-//     if(fast == NULL){               //---->2step question requirement
+//     if(fast == NULL){          //---->2step question requirement
 //         return NULL;
 //     }
 
-//     // yaha tk pochai, iska mtlab slow & fast meet kr chukai hai
-//     slow = head;                    //---->1step to check cyle present or not
-//     while(fast != slow){
+//     slow = head;              // yaha tk pochai, iska mtlab slow & fast meet kr chukai hai
+//     while(fast != slow){      //slow and fast->1step chlaiga
 //         slow = slow->next;
 //         fast = fast->next;
 //     }
-//     return slow;
+//     return slow;             //return starting point
 // }
 
-//------------------------------------------------------->LL in loop remove
-// ListNode *detectCycle(ListNode *head) {
-//     ListNode* slow = head;
-//     ListNode* fast = head;
 
-//     while(fast != NULL){             //---->1step to check cyle present or not
+
+
+//------------------------------------------------------->remove loop(code run nhi kr rha jaisai babbar dikha rha hai)
+// #include <iostream>
+// using namespace std;
+
+
+// class Node
+// {
+// public:
+//     int data;
+//     Node *next;
+//     Node()                //default constructor
+//     {
+//         this->data = 0;
+//         this->next = NULL;
+//     }
+//     Node(int data)        //parameterized constructor
+//     {
+//         this->data = data;
+//         this->next = NULL;
+//     }
+// };
+
+// void insertAtHead(Node* &head, Node* &tail, int data) {     //always pass by refrence
+//     //check for Empty LL case
+//     if(head == NULL) {
+//         Node* newNode = new Node(data); //step1: create new node
+//         head = newNode;                 //step2: update head
+//         tail = newNode;                 //step2: update tail
+//     }
+//     else {  //check for non Empty LL case
+//         //step1: create new node
+//         Node* newNode = new Node(data);
+//         //step2: attach new node to head node
+//         newNode -> next = head;
+//         //step3: update head
+//         head = newNode;
+//     }
+// }
+
+// void insertAtTail(Node* &head, Node* &tail, int data) {
+//     if(head == NULL) {  //empty LL case
+//         Node* newNode = new Node(data);  //step1: creatae a node
+//         head = newNode;   //step2: sinle node hai entrie list me, tahts why head and tail ko isper point krwado
+//         tail = newNode;
+//     }
+//     else {  //non-empty LL case
+//         //step1: creatae a node
+//         Node* newNode = new Node(data);
+//         //step2: tail node ko attach karo newnode se
+//         tail->next = newNode;
+//         //step3: update tail;
+//         tail = newNode;
+//     }
+// }
+
+// void print(Node* head) {
+
+//     Node* temp = head;     //for traversal orgenal pointer not used for best practise❤️
+//     while(temp != NULL) {
+//         cout << temp->data << "->";
+//         temp = temp->next;
+//     }
+// }
+
+// void* removeLoop(Node* &head) {
+//     //step1:check for loop
+//     Node* slow = head;
+//     Node* fast = head;
+
+//     while(fast != NULL){
 //         fast = fast->next;
 //         if(fast != NULL){
 //             fast = fast->next;
@@ -58,13 +127,12 @@
 //         if(fast == slow) break;
 //     }
 
-//     if(fast == NULL){               //---->2step question requirement
+//     if(fast == NULL){          //---->2step question requirement
 //         return NULL;
 //     }
 
-//     // yaha tk pochai, iska mtlab slow & fast meet kr chukai hai
-//     slow = head;                    //---->1step to check cyle present or not
-//     while(fast != slow){
+//     slow = head;              // yaha tk pochai, iska mtlab slow & fast meet kr chukai hai
+//     while(fast != slow){      //slow and fast->1step chlaiga
 //         slow = slow->next;
 //         fast = fast->next;
 //     }
@@ -80,188 +148,191 @@
 //     temp->next = NULL;
 // }
 
-//------------------------------------------------------->add 1 to ll
-#include <iostream>
-using namespace std;
+// Node* getStartingPointOfLoop(Node* &head) {
+//     //step1:check for loop
+//     Node* slow = head;
+//     Node* fast = head;
+//     while(fast != NULL){
+//         fast = fast->next;
+//         if(fast != NULL){
+//             fast = fast->next;
+//             slow = slow->next;
+//         }
+//         if(fast == slow) break;
+//     }
+//     if(fast == NULL){          //---->2step question requirement
+//         return NULL;
+//     }
+//     slow = head;              // yaha tk pochai, iska mtlab slow & fast meet kr chukai hai
+//     while(fast != slow){      //slow and fast->1step chlaiga
+//         slow = slow->next;
+//         fast = fast->next;
+//     }
+//     return slow;             //return starting point
+// }
 
-class Node
-{
-public:
-    int data;
-    Node *next;
+// int main() {
+//     Node* head = NULL;
+//     Node* tail = NULL;
+//     insertAtHead (head, tail,20);
+//     insertAtHead (head, tail, 50);
+//     insertAtHead (head, tail,60);
+//     insertAtHead (head, tail, 90);
+//     insertAtTail (head, tail, 77);
+//     tail -> next = head->next;
+    
+//     cout << getStartingPointOfLoop(head) -> data << endl;
+//     removeLoop(head);
+//     print(head); 
+//     return 0;
+// }
 
-    Node()
-    {
-        this->data = 0;
-        this->next = NULL;
-    }
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
-};
+//------------------------------------------------------->add 1 to a ll(GFG)
+// #include <iostream>
+// using namespace std;
 
-// I want to insert a node right at the head of Linked List
-void insertAtHead(Node *&head, Node *&tail, int data)
-{
-    // check for Empty LL
-    if (head == NULL)
-    {
-        Node *newNode = new Node(data);
-        head = newNode;
-        tail = newNode;
-    }
-    else
-    {
-        // step1:
-        Node *newNode = new Node(data);
-        // step2:
-        newNode->next = head;
-        // step3:
-        head = newNode;
-    }
-}
-// I want to insert a node right at the end of LINKED LIST
-void insertAtTail(Node *&head, Node *&tail, int data)
-{
-    if (head == NULL)
-    {
-        Node *newNode = new Node(data);
-        head = newNode;
-        tail = newNode;
-    }
-    else
-    {
-        // step1: creatae a node
-        Node *newNode = new Node(data);
-        // step2: connect woth tail ndoe
-        tail->next = newNode;
-        // step3: update tail;
-        tail = newNode;
-    }
-}
-void print(Node *head)
-{
+// class Node
+// {
+// public:
+//     int data;
+//     Node* next;
+//     Node()
+//     {
+//         this->data = 0;
+//         this->next = NULL;
+//     }
+//     Node(int data)
+//     {
+//         this->data = data;
+//         this->next = NULL;
+//     }
+// };
 
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-}
 
-int findLength(Node *&head)
-{
-    int len = 0;
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        temp = temp->next;
-        len++;
-    }
-    return len;
-}
+// void insertAtHead(Node* &head, Node* &tail, int data)
+// {
+//     // check for Empty LL
+//     if (head == NULL)
+//     {
+//         Node* newNode = new Node(data);
+//         head = newNode;
+//         tail = newNode;
+//     }
+//     else
+//     {
+//         // step1:
+//         Node* newNode = new Node(data);
+//         // step2:
+//         newNode->next = head;
+//         // step3:
+//         head = newNode;
+//     }
+// }
 
-void insertAtPosition(int data, int position, Node *&head, Node *&tail)
-{
-    int len = findLength(head);
+// void insertAtTail(Node* &head, Node* &tail, int data)
+// {
+//     if (head == NULL)
+//     {
+//         Node* newNode = new Node(data);
+//         head = newNode;
+//         tail = newNode;
+//     }
+//     else
+//     {
+//         // step1: creatae a node
+//         Node* newNode = new Node(data);
+//         // step2: connect woth tail ndoe
+//         tail->next = newNode;
+//         // step3: update tail;
+//         tail = newNode;
+//     }
+// }
 
-    if (position == 1)
-    {
-        insertAtHead(head, tail, data);
-        return;
-    }
-    else if (position > len)
-    {
-        insertAtTail(head, tail, data);
-        return;
-    }
-    else
-    {
-        Node *newNode = new Node(data);
+// void print(Node* head)
+// {
+//     Node* temp = head;
+//     while (temp != NULL)
+//     {
+//         cout << temp->data << " ";
+//         temp = temp->next;
+//     }
+// }
 
-        Node *prev = NULL;
-        Node *curr = head;
-        while (position != 1)
-        {
-            position--;
-            prev = curr;
-            curr = curr->next;
-        }
+// int findLength(Node* &head)
+// {
+//     int len = 0;
+//     Node* temp = head;
+//     while (temp != NULL)
+//     {
+//         temp = temp->next;
+//         len++;
+//     }
+//     return len;
+// }
 
-        // step3:
-        newNode->next = curr;
+// Node* reverse(Node *head){
+//     Node* prev = NULL;
+//     Node* curr = head;
 
-        // step4:
-        prev->next = newNode;
-    }
-}
+//     while(curr != NULL){
+//         Node* next = curr->next;
+//         curr->next = prev;
+//         prev = curr;
+//         curr = next;
+//     }
+//     return prev;
+// }
 
-Node* reverse(Node *head){
-    Node* prev = NULL;
-    Node* curr = head;
+// void add1(Node* &head){
+//     // reverse
+//     head = reverse(head);
+//     // adding
+//     int carry = 1;
+//     Node* temp = head;
+//     while(temp->next != NULL){
+//         int totalSum = temp->data + carry;
+//         int digit = totalSum % 10;
+//         carry = totalSum / 10;
 
-    while(curr != NULL){
-        Node* next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return prev;
-}
+//         temp->data = digit;
+//         temp = temp->next;
+//         if(carry == 0){
+//             break;  //additional use or not dont care
+//         }
+//     }
 
-void add1(Node *head){
-    // reverse
-    head = reverse(head);
-    // adding
-    int carry = 1;
-    Node* temp = head;
-    while(temp->next != NULL){
-        int totalSum = temp->data + carry;
-        int digit = totalSum % 10;
-        carry = totalSum / 10;
+//     // process of last node
+//     if(carry != 0){
+//         int totalSum = temp->data + carry;
+//         int digit = totalSum % 10;
+//         carry = totalSum / 10;
 
-        temp->data = digit;
-        temp = temp->next;
-        if(carry == 0){
-            break;  //additional use or not dont care
-        }
-    }
+//         temp->data = digit;
+//         if(carry != 0){
+//             Node* newNode = new Node(carry);
+//             temp->next = newNode;
+//         }
+//     }
 
-    // process of last node
-    if(carry != 0){
-        int totalSum = temp->data + carry;
-        int digit = totalSum % 10;
-        carry = totalSum / 10;
+//     // reverse
+//     head = reverse(head);
+// }
 
-        temp->data = digit;
-        if(carry != 0){
-            Node* newNode = new Node(carry);
-            temp->next = newNode;
-        }
-    }
+// int main()
+// {
 
-    // reverse
-    head = reverse(head);
-}
+//     Node *head = NULL;
+//     Node *tail = NULL;
+//     insertAtHead(head, tail, 9);
+//     insertAtHead(head, tail, 9);
+//     insertAtHead(head, tail, 9);
 
-int main()
-{
+//     print(head);
+//     cout << endl;
 
-    Node *head = NULL;
-    Node *tail = NULL;
-    insertAtHead(head, tail, 9);
-    insertAtHead(head, tail, 9);
-    insertAtHead(head, tail, 9);
+//     add1(head);
+//     print(head);
+//     cout << endl;
 
-    print(head);
-    cout << endl;
-
-    add1(head);
-    print(head);
-    cout << endl;
-
-    return 0;
-}
+//     return 0;
+// }
