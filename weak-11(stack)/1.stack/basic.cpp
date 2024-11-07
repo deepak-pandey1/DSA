0872860cf2f226d6984a5fab09f2cbaad8fafa92
deepak-pandey1 <deepakpandeyofficially@gmail.com>
2024-11-07 10:55:18 +0530

    stack 1 pdf

--- weak-11(stack)/1.stack/basic.cpp
+++ weak-11(stack)/1.stack/basic.cpp
@@ -1,4 +1,4 @@
-//------------------------------------------------------->BY STL(creation, remove, insert, check empty, check size, check top element)
+//------------------------------------------------------->C++ STL(creation, remove, insert, check empty, check size, check top element)
 // #include <iostream>
 // #include <stack>
 // using namespace std;
@@ -21,12 +21,10 @@
 //     st.pop();
 
 //     // check empty
-//     if (st.empty())
-//     {
+//     if (st.empty()) {
 //         cout << "Stack is empty" << endl;
 //     }
-//     else
-//     {
+//     else{
 //         cout << "stack is non empty" << endl;
 //     }
 
@@ -36,128 +34,108 @@
 //     return 0;
 // }
 
-//------------------------------------------------------->stack implementation by own
+//------------------------------------------------------->stack implementation
 // #include <iostream>
 // using namespace std;
 
-// class Stack
-// {
+// class Stack {
 // public:
-//     int *arr;
+//     int *arr;   //dynamic array kai liyai pointer use kisya hai
 //     int size;
 //     int top;
 
-//     Stack(int size)
-//     {
+//     Stack(int size) {                //for stack create
 //         arr = new int[size];
 //         this->size = size;
 //         this->top = -1;
 //     }
 
-//     void push(int data)
-//     {
-//         if (top == size - 1)
-//         {
+//     void push(int data) {            //for insert data
+//         if (top == size - 1) {
 //             cout << "Stack overflow" << endl;
 //             return;
 //         }
-//         else
-//         {
+//         else {
 //             top++;
 //             arr[top] = data;
 //         }
 //     }
 
-//     void pop()
-//     {
-//         if (top == -1)
-//         {
+//     void pop() {
+//         if (top == -1) {
 //             cout << "Stack underflow" << endl;
 //             return;
 //         }
-//         else
-//         {
+//         else {
 //             top--;
 //         }
 //     }
 
-//     bool isEmpty()
-//     {
-//         if (top == -1)
-//         {
+//     bool isEmpty() {
+//         if (top == -1) {
 //             return true;
 //         }
-//         else
-//         {
+//         else {
 //             return false;
 //         }
 //     }
 
-//     int getTop()
-//     {
-//         if (top == -1)
-//         {
+//     int getTop() {
+//         if (top == -1) {
 //             cout << "Stack is empty" << endl;
 //             return -1;
 //         }
-//         else
-//         {
+//         else {
 //             return arr[top];
 //         }
 //     }
 
-//     int getSize()
-//     {
+//     int getSize() {
 //         return top + 1;
 //     }
 
-//     void print()
-//     {
-//         cout << "Top:  " << top << endl;
+//     void print() { //print funcnality nhi hoti stack mai, this create for how our code run
+//         cout << "Top, like index:  " << top << endl;
 //         cout << "Top element: " << getTop() << endl;
 //         cout << "Stack: ";
-//         for (int i = 0; i < getSize(); i++)
-//         {
+//         for (int i = 0; i < getSize(); i++) {
 //             cout << arr[i] << " ";
 //         }
 //         cout << endl << endl;
 //     }
 // };
 
-// int main()
-// {
+// int main() {
 
-//     // creation
+//     // creation(static way of creation)
 //     Stack st(8);
 
-//     // push
-//     st.push(10);
+//     // // push
+//     // st.push(10);
+//     // // st.print();
+
+//     // st.push(20);
 //     // st.print();
 
-//     st.push(20);
-//     st.print();
+//     // cout << endl
+//     //      << "Top elemet: " << st.getTop() << endl;
+//     // cout << endl
+//     //      << "Size of stack: " << st.getSize() << endl;
 
-//     cout << endl
-//          << "Top elemet: " << st.getTop() << endl;
-//     cout << endl
-//          << "Size of stack: " << st.getSize() << endl;
-
-//     st.pop();
-//     st.pop();
-//     if (st.isEmpty())
-//     {
+//     // st.pop();
+//     // st.pop();
+//     if (st.isEmpty()) {
 //         cout << "Empty" << endl;
 //     }
-//     else
-//     {
+//     else {
 //         cout << "Not empty" << endl;
 //     }
 
-//     //   st.push(30);
-//     //   st.print();
+//       st.push(20);
+//       st.print();
 
-//     //   st.push(10);
-//     //   st.print();
+//       st.push(10);
+//       st.print();
 
 //     //   st.push(20);
 //     //   st.print();
@@ -165,8 +143,9 @@
 //     //   st.push(30);
 //     //   st.print();
 
-//     //   st.pop();
-//     //   st.print();
+//       st.pop();
+//       st.print();
+//       cout<< st.getSize();
 //     //   st.pop();
 //     //   st.print();
 //     //   st.pop();
@@ -175,100 +154,97 @@
 //     return 0;
 // }
 
-//------------------------------------------------------->reverse by stack
+// ------------------------------------------------------->reverse by stack
 // #include <iostream>
 // #include <stack>
 // using namespace std;
-// int main()
-// {
-//     string str = "hello jee";
+
+// int main() {
+//     string str = "hellojee";
 //     stack<char> st;
 
-//     for(int i = 0; i<str.length(); i++){
+//     for(int i = 0; i<str.length(); i++) {
 //         char ch = str[i];
 //         st.push(ch);
 //     }
 
-//     cout << endl;
-//     while(!st.empty()){
+//     while( !st.empty() ) {
 //         cout<<st.top();
 //         st.pop();
 //     }
 //     return 0;
 // }
 
-//------------------------------------------------------->find middle element
+// ------------------------------------------------------->find middle element of a stack
 // #include <iostream>
 // #include<stack>
 // using namespace std;
 
 // void solve(stack<int> &st, int& pos, int &ans) {
-//   //base case
-//   if(pos == 1) {
-//     ans =  st.top();
-//     // cout << "Deleting "<< st.top() << endl;
-//     // st.pop();
-//     return;
-//   }
-//   //1 case hum solve krenge
-//   pos--;
-//   int temp = st.top();
-//   st.pop();
-//   //recursion
-//   solve(st,pos,ans);
-//   //backtracjk
-//   st.push(temp);
+//     //base case
+//     if(pos == 1) {
+//         ans =  st.top();
+//         // st.pop();  //GFG pai delete krnai kai liyai dai rkha hai
+//         return;
+//     }
+//     //1 case hum solve krenge
+//     pos--;
+//     int temp = st.top();
+//     st.pop();
+//     //recursion
+//     solve(st,pos,ans);
+//     //backtracjk
+//     st.push(temp);
 // }
 
 // int getMiddleElement(stack<int> &st) {
-//   int size = st.size();
-//   if(st.empty()){
-//     cout << "Stack is empty, no middle element" << endl;
-//     return -1;
-//   }
-//   else {
-//     //stack is not empty
-//     //odd
-//     int pos = 0;
-//     if(size & 1) {
-//       pos = size/2 + 1;
+//     int size = st.size();
+//     if(st.empty()){
+//         cout << "Stack is empty, no middle element" << endl;
+//         return -1;
 //     }
-//     else {
-//       //even
-//       pos = size/2;
+//     else {    //stack is not empty
+//       int pos = 0;
+
+//       if(size & 1) {    //odd
+//         pos = size/2 + 1;
+//       }
+//       else {            //even
+//         pos = size/2;
+//       }
+//       int ans = -1;
+//       solve(st,pos,ans);
+//       return ans;
 //     }
-//     int ans = -1;
-//     solve(st,pos,ans);
-//     return ans;
-//   }
 // }
 
 // int main() {
-//   stack<int> st;
-//   st.push(10);
-//   st.push(20);
-//   st.push(30);
-//   st.push(50);
-//   st.push(60);
+//     stack<int> st;
 
-//   cout << "before size" << st.size() << endl;
-//   int mid = getMiddleElement(st);
+//     st.push(10);
+//     st.push(20);
+//     st.push(30);
+//     st.push(40);
+//     st.push(50);
+//     st.push(60);
 
-//   cout << "Middle element: " << mid << endl;
+//     // cout << "before size" << st.size() << endl;
+//     int mid = getMiddleElement(st);
 
-//   cout << "after size" << st.size() << endl;
-//   return 0;
+//     cout << "Middle element: " << mid << endl;
+
+//     // cout << "after size" << st.size() << endl;
+//     return 0;
 // }
 
-//------------------------------------------------------->insert at bottom
+//------------------------------------------------------->insert at bottom of stack
 // #include <iostream>
 // #include <stack>
 // using namespace std;
-// void insertAtBottom(stack<int> &st, int &element)
-// {
+
+// void insertAtBottom(stack<int> &st, int &element) {
 //     // base case
-//     if (st.empty())
-//     {
+//     if (st.empty()) {
 //         st.push(element);
 //         return;
 //     }
@@ -283,10 +259,9 @@
 //     // backTrack
 //     st.push(temp);
 // }
-// int main()
-// {
-//     stack<int> st;
 
+// int main() {
+//     stack<int> st;
 //     st.push(10);
 //     st.push(20);
 //     st.push(30);
@@ -295,56 +270,51 @@
 
 //     insertAtBottom(st, element);
 
-//     while (!st.empty())
-//     {
+//     while (!st.empty()) {
 //         cout << st.top() << " ";
 //         st.pop();
 //     }
 //     return 0;
 // }
 
-//------------------------------------------------------->reverse elemnt with this pattern by recursion
+//------------------------------------------------------->Reverse a stack(reverse elemnt with this pattern by recursion)
 // #include <iostream>
 // #include <stack>
 // using namespace std;
-// void insertAtBottom(stack<int> &st, int &element)
-// {
+
+// void insertAtBottom(stack<int> &st, int &element) {
 //     // base case
-//     if (st.empty())
-//     {
+//     if (st.empty()) {
 //         st.push(element);
 //         return;
 //     }
-
 //     // 1 case main solve karunga
 //     int temp = st.top();
 //     st.pop();
-
 //     // baaaki recursion
 //     insertAtBottom(st, element);
-
 //     // backTrack
 //     st.push(temp);
 // }
 
-// void reverseStack(stack<int> &st)
-// {
+// void reverseStack(stack<int> &st) {
 //     // base case
-//     if (st.empty())
-//     {
+//     if (st.empty()) {
 //         return;
 //     }
+
 //     // 1 case main solve krega
 //     int temp = st.top();
 //     st.pop();
+
 //     // refcursion
 //     reverseStack(st);
+
 //     // backtrack
 //     insertAtBottom(st, temp);
 // }
 
-// int main()
-// {
+// int main() {
 //     stack<int> st;
 
 //     st.push(10);
@@ -354,8 +324,7 @@
 //     reverseStack(st);
 
 //     cout<<"with reverse"<<endl;
-//     while (!st.empty())
-//     {
+//     while (!st.empty()) {
 //         cout << st.top() << " ";
 //         st.pop();
 //     }
@@ -367,9 +336,10 @@
 // #include <iostream>
 // #include <stack>
 // using namespace std;
+
 // void insertSorted(stack<int> &st, int element) {
 //   //base case
-//   if(st.empty() || element > st.top()) {
+//   if(st.empty() || element > st.top()) {  //jb bhi top access kr rhai ha so make sure satck empty to nhi hai
 //     st.push(element);
 //     return;
 //   }
@@ -377,8 +347,10 @@
 //   //1 case sambhalna h
 //   int temp = st.top();
 //   st.pop();
+
 //   //recursion sambhalega
 //   insertSorted(st,element);
+
 //   //backtrack
 //   st.push(temp);
 // }
@@ -393,8 +365,7 @@
 
 //     insertSorted(st, 25);
 
-//     while (!st.empty())
-//     {
+//     while (!st.empty()) {
 //         cout << st.top() << " ";
 //         st.pop();
 //     }
@@ -402,60 +373,58 @@
 // }
 
 
-//------------------------------------------------------->insert in a sorted stack
-#include <iostream>
-#include <stack>
-using namespace std;
+//------------------------------------------------------->sorte a stack
+// #include <iostream>
+// #include <stack>
+// using namespace std;
 
-void insertSorted(stack<int> &st, int element) {
-  //base case
-  if(st.empty() || element > st.top()) {
-    st.push(element);
-    return;
-  }
+// void insertSorted(stack<int> &st, int element) {
+//   //base case
+//   if(st.empty() || element > st.top()) {
+//     st.push(element);
+//     return;
+//   }
+//   //1 case sambhalna h
+//   int temp = st.top();
+//   st.pop();
+//   //recursion sambhalega
+//   insertSorted(st,element);
+//   //backtrack
+//   st.push(temp);
+// }
 
-  //1 case sambhalna h
-  int temp = st.top();
-  st.pop();
-  //recursion sambhalega
-  insertSorted(st,element);
-  //backtrack
-  st.push(temp);
-}
+// void sortStack(stack<int> &st) {
+//     //base case
+//     if(st.empty()) {
+//       return;
+//     }
 
-void sortStack(stack<int> &st) {
-  //base case
-  if(st.empty()) {
-    return;
-  }
+//     //1 case
+//     int temp = st.top();
+//     st.pop();
 
-  //1 case
-  int temp = st.top();
-  st.pop();
+//     //recursion
+//     sortStack(st);
 
-  //recursion
-  sortStack(st);
+//     //backtrack
+//     insertSorted(st, temp);
+// }
 
-  //backtrack
-  insertSorted(st, temp);
-}
+// int main()
+// {
+//     stack<int> st;
 
-int main()
-{
-    stack<int> st;
+//     st.push(10);
+//     st.push(7);
+//     st.push(12);
+//     st.push(5);
+//     st.push(11);
 
-    st.push(10);
-    st.push(7);
-    st.push(12);
-    st.push(5);
-    st.push(11);
+//     sortStack(st);
 
-    sortStack(st);
-
-    while (!st.empty())
-    {
-        cout << st.top() << " ";
-        st.pop();
-    }
-    return 0;
-}
+//     while (!st.empty()) {
+//         cout << st.top() << " ";
+//         st.pop();
+//     }
+//     return 0;
+// }
