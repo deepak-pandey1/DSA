c0c1dfda76634f2e70def1d24aef36e9650f4f6d
deepak-pandey1 <deepakpandeyofficially@gmail.com>
2024-11-07 14:29:03 +0530

    stack2

--- weak-11(stack)/2.stack/stack.cpp
+++ weak-11(stack)/2.stack/stack.cpp
@@ -1,114 +1,86 @@
-//------------------------------------------------------->implemented to stack in an array
-// #include <iostream>
-// using namespace std;
-// class Stack{
-//     public:
-//     int *arr;
-//     int size;
-//     int top1;
-//     int top2;
-
-//     Stack(int size){
-//         arr = new int[size];
-//         this->size = size;
-//         top1 = -1;
-//         top2 = size;
-//     }
-
-//     void push1(int data){
-//         if(top2-top1 == 1){
-//             cout<<"OVERFLOW"<<endl;
-//         }
-//         else{
-//             top1++;
-//             arr[top1] = data;
-//         }
-//     }
-//     void push2(int data){
-//         if(top2-top1 == 1){
-//             cout<<"OVERFLOW"<<endl;
-//         }
-//         else{
-//             top2--;
-//             arr[top2] = data;
-//         }
-//     }
-
-//     void pop1(){
-//         if(top1 == -1){
-//             cout<<"UNDERFLOW"<<endl;
-//         }
-//         else{
-//             arr[top1] = 0;
-//             top1--;
-//         }
-//     }
-
-//     void pop2(){
-//         if(top1 == size){
-//             cout<<"UNDERFLOW"<<endl;
-//         }
-//         else{
-//             arr[top2] = 0;
-//             top2++;
-//         }
-//     }
-
-//     // usually not present in stack but for chek
-//     void print(){
-//         cout<<endl;
-//         cout<<"Top1: "<<top1<<endl;
-//         cout<<"Top2: "<<top2<<endl;
-//         for(int i = 0; i<size; i++){
-//             cout<<arr[i]<<" ";
-//         }
-//         cout<<endl;
-//     }
-// };
-
-// int main()
-// {
-//     Stack st(6);         //6 is size
-//     st.print();
-
-//     st.push1(10);
-//     st.print();
-    
-//     st.push1(20);
-//     st.print();
-
-//     st.push1(30);
-//     st.print();
-
-//     st.push2(100);
-//     st.print();
-
-//     st.push2(900);
-//     st.print();
-
-//     st.push2(800);
-//     st.print();
-
-//     st.push2(800);
-//     st.print();
-
-//     st.pop1();
-//     st.print();
-//     st.pop1();
-//     st.print();
-//     st.pop1();
-//     st.print();
-//     st.pop1();
-//     st.print();
-//     return 0;
-// }
-
-
-//------------------------------------------------------->remove redundents bracket
+//------------------------------------------------------->implemented 2 stack in an array
 #include <iostream>
 using namespace std;
-int main()
-{
-    cout << "hello world";
+
+class Stack {
+    public:
+        int *arr;
+        int size;
+        int top1;
+        int top2;
+
+    Stack(int size) {
+        arr = new int[size];
+        this->size = size;
+        top1 = -1;
+        top2 = size;
+    }
+
+    void push1(int data) {
+        if(top2-top1 == 1) {        //no space available
+            cout<<"OVERFLOW"<<endl;
+        }
+        else{
+            top1++;
+            arr[top1] = data;
+        }
+    }
+    void push2(int data) {
+        if(top2-top1 == 1) {
+            cout<<"OVERFLOW"<<endl;
+        }
+        else{
+            top2--;
+            arr[top2] = data;
+        }
+    }
+
+    void pop1() {
+        if(top1 == -1) {             //stack1 is empty
+            cout<<"UNDERFLOW"<<endl;
+        }
+        else{
+            arr[top1] = 0;
+            top1--;
+        }
+    }
+
+    void pop2() {
+        if(top1 == size) {
+            cout<<"UNDERFLOW"<<endl;
+        }
+        else{
+            arr[top2] = 0;
+            top2++;
+        }
+    }
+
+    // usually not present in stack but for chek
+    void print(){
+        cout<<endl;
+        cout<<"Top1: "<<top1<<endl;
+        cout<<"Top2: "<<top2<<endl;
+        for(int i = 0; i<size; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+int main() {
+    Stack st(6);         //6 is size
+
+    st.push1(10);
+    st.push1(20);
+    st.push1(30);
+
+    st.push2(100);
+    st.push2(900);
+    st.push2(800);
+
+    st.print();
+
+    st.pop1();
+    st.print();
     return 0;
 }
