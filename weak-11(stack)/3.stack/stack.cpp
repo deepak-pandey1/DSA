c2708dfe75edfafedb13170eac95f9921a133d80
deepak-pandey1 <deepakpandeyofficially@gmail.com>
2024-11-08 11:00:16 +0530

    stacke 3

--- weak-11(stack)/3.stack/stack.cpp
+++ weak-11(stack)/3.stack/stack.cpp
@@ -3,11 +3,49 @@
 // #include <vector>
 // #include <stack>
 // using namespace std;
-// vector<int> nextSmallerElement(int arr[], int size, vector<int> &ans){
+
+// vector<int> nextSmallerElement(int arr[], int size, vector<int> &ans) {
 //     stack<int> st;
 //     st.push(-1);
 
-//     for(int i = size-1; i>=0; i--){
+//     for(int i = size-1; i >= 0; i--) {
+//         int curr = arr[i];
+//         // answer find kro curr kai liyai
+//         while(st.top() >= curr) {
+//             st.pop();
+//         }
+
+//         ans[i] = st.top();
+//         st.push(curr);
+//     }
+//     return ans;
+// }
+// int main() {
+//     int arr[5] = {8,4,6,2,3};
+//     int size = 5;
+
+//     vector<int> ans(size);
+//     ans = nextSmallerElement(arr, size, ans);
+
+//     for(auto i: ans) {
+//         cout<<i<<" ";
+//     }
+//     cout<<endl;
+
+//     return 0;
+// }
+
+
+//------------------------------------------------------->prev smaller element
+// #include <iostream>
+// #include <vector>
+// #include <stack>
+// using namespace std;
+// vector<int> prevSmallerElement(int arr[], int size, vector<int> &ans){
+//     stack<int> st;
+//     st.push(-1);
+
+//     for(int i = 0; i<size; i++){
 //         int curr = arr[i];
         
 //         // answer find kro curr kai liyai
@@ -20,13 +58,12 @@
 //     }
 //     return ans;
 // }
-// int main()
-// {
+// int main() {
 //     int arr[5] = {8,4,6,2,3};
 //     int size = 5;
 
 //     vector<int> ans(size);
-//     ans = nextSmallerElement(arr, size, ans);
+//     ans = prevSmallerElement(arr, size, ans);
 
 //     for(auto i: ans){
 //         cout<<i<<" ";
@@ -35,42 +72,3 @@
 
 //     return 0;
 // }
-
-
-//------------------------------------------------------->prev smaller element
-#include <iostream>
-#include <vector>
-#include <stack>
-using namespace std;
-vector<int> prevSmallerElement(int arr[], int size, vector<int> &ans){
-    stack<int> st;
-    st.push(-1);
-
-    for(int i = 0; i<size; i++){
-        int curr = arr[i];
-        
-        // answer find kro curr kai liyai
-        while(st.top() >= curr){
-            st.pop();
-        }
-
-        ans[i] = st.top();
-        st.push(curr);
-    }
-    return ans;
-}
-int main()
-{
-    int arr[5] = {8,4,6,2,3};
-    int size = 5;
-
-    vector<int> ans(size);
-    ans = prevSmallerElement(arr, size, ans);
-
-    for(auto i: ans){
-        cout<<i<<" ";
-    }
-    cout<<endl;
-
-    return 0;
-}
